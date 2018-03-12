e9dfcf5451bcebbe0159d2321a07a7ebe8f83241
Exellen <ab.zsolt.sz@gmail.com>
2018-03-12 19:33:17 +0100

    Created "MyCode" script

---
+++ Assets/MyCode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCode : MonoBehaviour {
+    
+	void Start () {
+        print("You found the castle");
+	}
+
+	void Update () {
+		
+	}
+}
