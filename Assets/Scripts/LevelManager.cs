ff6508a6cd2361358be5abc93bed624a6b78d2a3
Exellen <ab.zsolt.sz@gmail.com>
2018-03-13 19:52:13 +0100

    Unity: Created basic Menu System with placeholders

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager: MonoBehaviour {
+    
+	void Start ()
+    {
+        Invoke("LoadNextScene", 1.0f);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit requested!");
+    }
+}
